16e0d0db3806d0b5b08ec613f7f6c7d8e780ee97
Dajcochran <Dajcochran@gmail.com>
2020-11-28 10:25:46 -0500

    swapped staff photo to jpg

--- src/components/OurStaff.jsx
+++ src/components/OurStaff.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, Container, Row, Col } from 'react-bootstrap'
-import staff from '../images/staff.webp'
+import staff from '../images/staff.jpg'
 const OurStaff = () => {
     return (
             <div style={{backgroundColor:"maroon"}}>
